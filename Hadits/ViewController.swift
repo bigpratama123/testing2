ceebeb94ca9b8a17157353a7200c6f37630d8fea
Risqi <pratamabig94@gmail.com>
2020-07-27 08:25:09 +0700

    Initial Commit

---
+++ Hadits/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Hadits
+//
+//  Created by MSI-H81-E33 on 27/07/20.
+//  Copyright © 2020 MSI-H81-E33. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
